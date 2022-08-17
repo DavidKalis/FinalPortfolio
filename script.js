6a4eb7d4b8e939ee89ef0ccdd1d94e9ddb88586a
DavidKalis <dawek977@gmail.com>
2022-08-17 03:24:44 +0200

    contact fix

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 "e.preventDefault();"
 
-const { EmailJSResponseStatus } = require("@emailjs/browser");
 
 const menuToggle = document.querySelector (".menuToggle");
 const navigation = document.querySelector(".navigation");
@@ -22,10 +21,10 @@ list.forEach((item) => item.addEventListener("click", activeLink));
 let publicKey = "fixJgTQRCqSyzhwQl";
 function sendMail(params) {
     let tempParams = {
-        contact_form_subject: document.getElementByClass("contact-form-subject").value,
-        contact_form_name: document.getElementByClass("contact-form-name").value,
-        contact_form_email: document.getElementByClass("contact-form-email").value,
-        contact_form_message: document.getElementByClass("contact-form-message").value,
+        contact_form_subject: document.getElementById("sub").value,
+        contact_form_name: document.getElementById("name").value,
+        contact_form_email: document.getElementById("mail").value,
+        contact_form_message: document.getElementById("msg").value,
     };
 
     emailjs.send("service_8081u2r", "template_g3i821h", tempParams, publicKey).then(
